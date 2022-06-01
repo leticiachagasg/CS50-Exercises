b5692928b98134bbf0a52bb272f2e7f5712c3f9f
leticiachagasg <97345497+leticiachagasg@users.noreply.github.com>
2022-06-01 09:05:32 -0300

    Week 1 Exercise - Mario (more)
    Exercise from CS50 - Problem Set 1 - Week 1
    Mario ("more comfortable" version)
    https://cs50.harvard.edu/x/2022/psets/1/mario/more/

---
+++ mario-more/mario.c
@@ -0,0 +1,54 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+    int h;
+
+// get the height of the pyramid - number must be between 1 and 8 inclusive
+
+    do
+    {
+        h = get_int("Height: ");
+    }
+    while (h < 1 || h > 8);
+
+
+    int spaces = h - 1;
+
+//for loop to print number of lines = heigh
+
+    for (int c = 0; c < h; c++)
+    {
+// print number of spaces on the line (which will start as the number of heigh - 1, as defined in the variable spaces)
+
+        for (int i = spaces; i != 0; i--)
+        {
+            printf(" ");
+        }
+
+//print number of blocks on each line (which will be the heigh - spaces)
+
+        for (int j = (h - spaces); j != 0; j--)
+        {
+
+            printf("#");
+        }
+
+// add the space between the pyramids (always 2) + repeat the blocks for each line
+
+        printf("  ");
+
+        for (int k = (h - spaces); k != 0; k--)
+        {
+            printf("#");
+        }
+
+//when you subtract one space from the line, you will increase one block (because blocks = heigh - spaces)
+
+        spaces--;
+        printf("\n");
+
+    }
+}
